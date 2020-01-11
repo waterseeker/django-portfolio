838047165a06607ffa367bfd4d6ca399af27eebc
wayne <you@example.com>
2020-01-11 17:08:57 -0600

    added blog, fixed card size on projects index

--- core/urls.py
+++ core/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
+    path("blog/", include("blog.urls")),
 ] 
 
 if settings.DEBUG:
