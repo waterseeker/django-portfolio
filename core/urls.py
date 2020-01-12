85a192fc69c793a69087d06a70fab8cafce8dcc6
wayne <you@example.com>
2020-01-12 09:17:05 -0600

    added blank langing page

--- core/urls.py
+++ core/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("projects/", include("projects.urls")),
     path("blog/", include("blog.urls")),
+    path("", views.landing_page, name="landing_page"),
 ] 
 
 if settings.DEBUG:
